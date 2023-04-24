493a348c48765e6b71bb36c4f103cf51f7d56919
changmanuel14 <changmanuel14@gmail.com>
2023-04-24 10:04:13 -0600

    Se filtran los estudiantes de auxiliar de enfermeria que no se hayan retirado

--- flaskapp/web.py
+++ flaskapp/web.py
@@ -321,7 +321,7 @@ def repauxenf():
 		try:
 			with conexion.cursor() as cursor:
 				datagen = []
-				consulta = f"SELECT p.nombre, p.carnet from pagos p inner join codigos c on c.idcodigos = p.idcod where c.concepto like '%Mensualidad Auxiliar de enfermeria%' group by p.nombre order by p.nombre;"
+				consulta = f"SELECT p.nombre, p.carnet from pagos p inner join codigos c on c.idcodigos = p.idcod where c.concepto like '%Inscripción Auxiliar de enfermeria%' and c.descripcion not like '%Retirado%' group by p.nombre order by p.nombre;"
 				cursor.execute(consulta)
 				nombres = cursor.fetchall()
 				datos = []
