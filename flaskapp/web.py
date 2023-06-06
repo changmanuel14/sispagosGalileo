59907e15459a206085d5792d02ced3bf409f351b
changmanuel14 <changmanuel14@gmail.com>
2023-06-06 16:50:51 -0600

    Recalculo de mora

--- flaskapp/web.py
+++ flaskapp/web.py
@@ -341,7 +341,7 @@ def repauxenfexcel():
 		try:
 			with conexion.cursor() as cursor:
 				datagen = []
-				consulta = f"SELECT p.nombre, p.carnet from pagos p inner join codigos c on c.idcodigos = p.idcod where c.concepto like '%Mensualidad Auxiliar de enfermeria%' group by p.nombre order by p.nombre;"
+				consulta = f"SELECT p.nombre, p.carnet from pagos p inner join codigos c on c.idcodigos = p.idcod where c.concepto like '%Inscripción Auxiliar de enfermeria%' and p.extra not like '%Retirado%' group by p.nombre order by p.nombre;"
 				cursor.execute(consulta)
 				nombres = cursor.fetchall()
 				datos = []
